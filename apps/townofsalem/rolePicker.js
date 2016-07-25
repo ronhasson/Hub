d909aff9fc1ff7b2120d9c3dfa989b2b6667b5d6
RoyIsland <roy1.island@gmail.com>
2016-07-25 14:40:28 +0300

    added functions

--- apps/townofsalem/rolePicker.js
+++ apps/townofsalem/rolePicker.js
@@ -9,7 +9,7 @@ exports.getRoles = function getRoles(num) {
                 randomizeRandomTown(),
                 Role.Godfather,
                 Role.Mafioso
-                randomizeRandomBenignOrEvil()
+                randomizeBenignOrEvil()
             ];
             break;
     }
@@ -19,13 +19,74 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function randomizeRandomTown() {
+    switch (randomInt(0, 13)) {
+        case 0:
+            return Role.Investigator;
+        case 1:
+            return Role.Lookout;
+        case 2:
+            return Role.Sheriff;
+        case 3:
+            return Role.Spy;
+        case 4:
+            return Role.VampireHunter;
+        case 5:
+            return Role.Veteran;
+        case 6:
+            return Role.Vigilante;
+        case 7:
+            return Role.Bodyguard;
+        case 8:
+            return Role.Doctor;
+        case 9:
+            return Role.Escort;
+        case 10:
+            return Role.Mayor;
+        case 11:
+            return Role.Medium;
+        case 12:
+            return Role.Retributionist;
+        case 13:
+            return Role.Transporter;
+    }
+}
+
 function randomizeTownInvestigative() {
     switch (randomInt(0, 2)) {
         case 0:
             return Role.Investigator;
         case 1:
-            return Role.Sheriff;
+            return Role.Lookout;
         case 2:
+            return Role.Sheriff;
+        case 3:
             return Role.Spy;
+    }
+}
+
+function randomizeTownSupport() {
+    switch (randomInt(0, 4)) {
+        case 0:
+            return Role.Escort;
+        case 1:
+            return Role.Mayor;
+        case 2:
+            return Role.Medium;
+        case 3:
+            return Role.Retributionist;
+        case 4:
+            return Role.Transporter;
+    }
+}
+
+function randomizeTownKilling() {
+    switch (randomInt(0, 2)) {
+        case 0:
+            return Role.VampireHunter;
+        case 1:
+            return Role.Veteran;
+        case 2:
+            return Role.Vigilante;
     }
 }
