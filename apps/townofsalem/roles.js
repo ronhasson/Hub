9341c45dc6daa80707d05d441c8b8a0f5af09f19
Ron <ron23231@gmail.com>
2016-07-26 19:24:28 +0300

    added more values to the roles

--- apps/townofsalem/roles.js
+++ apps/townofsalem/roles.js
@@ -1,38 +1,174 @@
 var Role = {
-    Bodyguard: "Bodyguard",
-    Doctor: "Doctor",
-    Escort: "Escort",
-    Investigator: "Investigator",
-    Jailor: "Jailor",
-    Lookout: "Lookout",
-    Mayor: "Mayor",
-    Medium: "Medium",
-    Retributionist: "Retributionist",
-    Sheriff: "Sheriff",
-    Spy: "Spy",
-    Transporter: "Transporter",
-    VampireHunter: "Vampire Hunter",
-    Veteran: "Veteran",
-    Vigilante: "Vigilante",
-    Blackmailer: "Blackmailer",
-    Consigliere: "Consigliere",
-    Consort: "Consort",
-    Disguiser: "Disguiser",
-    Forger: "Forger",
-    Framer: "Framer",
-    Godfather: "Godfather",
-    Janitor: "Janitor",
-    Mafioso: "Mafioso",
-    Amnesiac: "Amnesiac",
-    Arsonist: "Arsonist",
-    Executioner: "Executioner",
-    Jester: "Jester",
-    SerialKiller: "Serial Killer",
-    Survivor: "Survivor",
-    Vampire: "Vampire",
-    Werewolf: "Werewolf",
-    Witch: "Witch",
-    Troll: "Troll"
+    Bodyguard: {
+        name: "Bodyguard",
+        canKill: true,
+        team: "town"
+    },
+    Doctor: {
+        name: "Doctor",
+        canKill: false,
+        team: "town"
+    },
+    Escort: {
+        name: "Escort",
+        canKill: false,
+        team: "town"
+    },
+    Investigator: {
+        name: "Investigator",
+        canKill: false,
+        team: "town"
+    },
+    Jailor: {
+        name: "Jailor",
+        canKill: true,
+        team: "town"
+    },
+    Lookout: {
+        name: "Lookout",
+        canKill: false,
+        team: "town"
+    },
+    Mayor: {
+        name: "Mayor",
+        canKill: false,
+        team: "town"
+    },
+    Medium: {
+        name: "Medium",
+        canKill: false,
+        team: "town"
+    },
+    Retributionist: {
+        name: "Retributionist",
+        canKill: false,
+        team: "town"
+    },
+    Sheriff: {
+        name: "Sheriff",
+        canKill: false,
+        team: "town"
+    },
+    Spy: {
+        name: "Spy",
+        canKill: false,
+        team: "town"
+    },
+    Transporter: {
+        name: "Transporter",
+        canKill: false,
+        team: "town"
+    },
+    VampireHunter: {
+        name: "Vampire Hunter",
+        canKill: true,
+        team: "town"
+    },
+    Veteran: {
+        name: "Veteran",
+        canKill: true,
+        team: "town"
+    },
+    Vigilante: {
+        name: "Vigilante",
+        canKill: true,
+        team: "town"
+    },
+    Blackmailer: {
+        name: "Blackmailer",
+        canKill: false,
+        team: "mafia"
+    },
+    Consigliere: {
+        name: "Consigliere",
+        canKill: false,
+        team: "mafia"
+    },
+    Consort: {
+        name: "Consort",
+        canKill: false,
+        team: "mafia"
+    },
+    Disguiser: {
+        name: "Disguiser",
+        canKill: false,
+        team: "mafia"
+    },
+    Forger: {
+        name: "Forger",
+        canKill: false,
+        team: "mafia"
+    },
+    Framer: {
+        name: "Framer",
+        canKill: false,
+        team: "mafia"
+    },
+    Godfather: {
+        name: "Godfather",
+        canKill: true,
+        team: "mafia"
+    },
+    Janitor: {
+        name: "Janitor",
+        canKill: false,
+        team: "mafia"
+    },
+    Mafioso: {
+        name: "Mafioso",
+        canKill: true,
+        team: "mafia"
+    },
+    Amnesiac: {
+        name: "Amnesiac"
+        canKill: false,
+        team: "neutral"
+    },
+    Arsonist: {
+        name: "Arsonist"
+        canKill: true,
+        team: "neutral"
+    },
+    Executioner: {
+        name: "Executioner"
+        canKill: false,
+        team: "neutral"
+    },
+    Jester: {
+        name: "Jester"
+        canKill: false,
+        team: "neutral"
+    },
+    SerialKiller: {
+        name: "Serial Killer"
+        canKill: true,
+        team: "neutral"
+    },
+    Survivor: {
+        name: "Survivor"
+        canKill: false,
+        team: "neutral"
+    },
+    Vampire: {
+        name: "Vampire"
+        canKill: false,
+        team: "neutral"
+    },
+    Werewolf: {
+        name: "Werewolf"
+        canKill: true,
+        team: "neutral"
+    },
+    Witch: {
+        name: "Witch"
+        canKill: false,
+        team: "neutral"
+    },
+    Troll: {
+        name: "Troll"
+        canKill: false,
+        team: "neutral"
+    }
 };
 
 module.exports = Role;
