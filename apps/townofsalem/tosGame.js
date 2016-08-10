50d6db8280eca0008b28cf042ba84b6288e198b1
committedninja <shohamh@hotmail.com>
2016-08-10 20:46:06 +0300

    I pressed reformat code before doing anything.  and it added spaces like this { }. so yes.

--- apps/townofsalem/tosGame.js
+++ apps/townofsalem/tosGame.js
@@ -1,27 +1,27 @@
 function firstDayStart() {
-    setTimeout(function() {}, 5000);
+  setTimeout(function () { }, 5000);
 }
 
-function discussion(){
-  setTimeout(function() {}, 45000);
+function discussion() {
+  setTimeout(function () { }, 45000);
 }
 
-function voting(){
-  setTimeout(function() {}, 30000);
+function voting() {
+  setTimeout(function () { }, 30000);
 }
 
-function defense(){
-  setTimeout(function() {}, 20000);
+function defense() {
+  setTimeout(function () { }, 20000);
 }
 
-function judgement(){
-  setTimeout(function() {}, 20000);
+function judgement() {
+  setTimeout(function () { }, 20000);
 }
 
-function lastWords(){
-  setTimeout(function() {}, 5000);
+function lastWords() {
+  setTimeout(function () { }, 5000);
 }
 
-function night(){
-  setTimeout(function() {}, 40000);
+function night() {
+  setTimeout(function () { }, 40000);
 }
