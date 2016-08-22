08b1133ddbd02857715d170cdea1fce3405e314a
Jay <jayleeman12@gmail.com>
2016-08-22 21:18:15 +0300

    take it

--- apps/townofsalem/tosGame.js
+++ apps/townofsalem/tosGame.js
@@ -1,69 +1,98 @@
 
 var dayOne = true;
 var voteCounter = 0;
-//save which phase you're on'
+var phase = "Discussion";
+//save which phase you're on
 function discussion(){
-	console.log("now in discussion");
-	voteCounter = 0;
+	phase = "Discussion";
 	var timer;
 	if(dayOne)
 		timer = 15000;
 	else
 		timer = 45000;
 	sendEmit("changePhase", {time: (timer / 1000), phase: "Discussion"});
-  setTimeout(function() {
-    if(dayOne)
-	{
-    	night();
-			dayOne = false;
-	}
-	else
-	{
-		voting();
-	}
-  }, timer);
+
+	var interval = setInterval(function() {
+            if (--timer < 0) {
+							clearInterval(interval);
+							if(dayOne) {
+								night();
+								dayOne = false;
+							}		
+							else {
+								voting();
+							}
+							return;
+            }
+        }, 1000);
 }
 
 function voting(){ //everyone alive can vote
+	phase = "Voting";
 	sendEmit("changePhase", {time: 30, phase: "Voting"});
-	if(voteCounter < 6)
-	{
-		console.log("NOW VOTING for the: " + voteCounter + "time");
-	}
-	else
-	{
-		console.log("voting ended, nightime");
-		night();
-	}
-  setTimeout(function() {
-	  defense();
-  }, 30000);
+	var timer = 30;
+	var interval = setInterval(function() {
+					if (--timer < 0) {
+						clearInterval(interval);
+						defense();
+						return;
+					}
+			}, 1000);
 }
 
 function defense(){ //only trailled person can talk
-  setTimeout(function() {
-	  judgement();
-  }, 20000);
+	phase = "Defense";
+	sendEmit("changePhase", {time: 20, phase: "Defense"});
+	var timer = 20;
+	var interval = setInterval(function() {
+			if (--timer < 0) {
+				clearInterval(interval);
+				judgement();
+				return;
+			}
+	}, 1000);
 }
 
 function judgement(){ //everyone alive can vote
-  setTimeout(function() {
-	  //if(guilty) last words
-	  //else voting
-  }, 20000);
+   phase = "Judgement";
+  sendEmit("changePhase", {time: 20, phase: "Judgement"});
+  var timer = 20;
+  var interval = setInterval(function() {
+			if (--timer < 0) {
+				clearInterval(interval);
+				lastWords();
+				return;
+			}
+	}, 1000);
 }
 
 function lastWords(){
-  setTimeout(function() {
-	  night();
-  }, 5000);
+	 	phase = "Last Words";
+		sendEmit("changePhase", {time: 5, phase: "Last Words"});
+		var timer = 5;
+		var interval = setInterval(function() {
+					if (--timer < 0) {
+						clearInterval(interval);
+						night();
+						return;
+					}
+			}, 1000);
 }
 
-function night(){
-
-  setTimeout(function() {}, 40000);
+function night() {
+	phase = "Night";
+  sendEmit("changePhase", {time: 40, phase: "Night"});
+  var timer = 40;
+  var interval = setInterval(function() {
+			if (--timer < 0) {
+				clearInterval(interval);
+				discussion();
+				return;
+			}
+	}, 1000);
 }
 
 module.exports = {
-    discussion: discussion
+    discussion: discussion,
+	phase: phase
 }
