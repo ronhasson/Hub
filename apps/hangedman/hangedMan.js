07107e0a77d64bdb77e5b8755045b9b5bae4943d
Ron <ron23231@gmail.com>
2016-08-31 16:26:22 +0300

    started again the works on the hangedman game

--- apps/hangedman/hangedMan.js
+++ apps/hangedman/hangedMan.js
@@ -1,5 +1,4 @@
 var http = require('http');
-var q = require('q');
 var request = require('request');
 
 var categories = new Array();
@@ -18,28 +17,35 @@ categories["food"] = 49;
 
 var chosenCateg = "science";
 
-var getClue = function() {
-    var defered = q.defer();
-    request("http://jservice.io/api/category?id="+categories[chosenCateg], function(err, resp, body) {
+function getQuestion(callback) {
+    var selected, categ;
+    request("http://jservice.io/api/category?id=" + categories[chosenCateg], function(err, resp, body) {
         if (!err && resp.statusCode === 200) {
             var json = JSON.parse(body);
+            categ = json.title;
             console.log(json);
-            var selected = json.clues[Math.floor(Math.random() * json.clues.length)];
+            selected = json.clues[Math.floor(Math.random() * json.clues.length)];
             console.log(selected);
-            defered.resolve(selected[0]);
-            console.log(json.length);
-        }
-        else {
-          console.log("error " + "http://jservice.io/api/category?id="+categories[chosenCateg])
+            callback(formatQ(selected, categ));
+        } else {
+            console.log("error " + "http://jservice.io/api/category?id=" + categories[chosenCateg]);
+            getQuestion();
         }
     });
-    return defered.promise;
 }
 
-var clue = getClue().then(function(myClue) {
-    console.log(myClue);
-    var clueid = myClue.id;
-    var question = myClue.question;
-    var category = myClue.category.title;
-    var answer = myClue.answer;
+function formatQ(q, title)
+{
+  var qObj = {
+    category : title,
+    question : q.question,
+    answer : q.answer,
+    points : q.value
+  }
+  console.log(qObj)
+  return qObj;
+}
+
+getQuestion(function(returnedQ){
+  console.log(returnedQ);
 });
